a998809786e67e70ec34ac2af3ba511da99f5db2
simeonoff <simeonoff@me.com>
2015-04-10 13:54:02 +0300

    added helpers

--- js/report.js
+++ js/report.js
@@ -53,11 +53,14 @@ function executeFileSave() {
     $("#downloadReportBtn").attr("class", "btn-floating btn-large waves-effect waves-light modal-trigger");
     $("#cancelProgress").hide();
     $("#downloadReportBtn").one("click", function(event) {
-
         event.preventDefault();
         //TODO check for number of items and limit it to 25000
         if (rows.length > 25000) {
-            $('#warnItemLimit').openModal();
+            $('#warnItemLimit').openModal({
+            	complete: function() {
+            		$("#downloadReportBtn").attr("class", "btn-floating btn-large disabled");
+            	} // Callback for Modal close
+            });
         } else {
             if (!!window.Worker) {
                 var worker = new Worker("../js/saveFileWorker.js");
@@ -179,6 +182,7 @@ var getFiles = function() {
 
 $(document).ready(function() {
     $('.modal-trigger').leanModal();
+    $("#instrContent").load("../helpers/instructions.html");
     $("#version").find(">a").html(VERSION);
     $(".button-collapse").sideNav();
     $("#cancelProgress").hide();
@@ -410,11 +414,283 @@ function getDocumentInfo() {
     };
 }
 
+// function getDocInfo() {
+
+//     return function() {
+//         //$(this.data.responseXML).SPFilterNode("z:row").SPXmlToJson();
+//         //assign json data object for the list to the data variable
+//         var data = this.data;
+//         //find email in address book contact
+//         //var regExEmail = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/i);
+//         //iterate the data
+//         for (var i = 0; i < data.length; i++) {
+
+//             catchError();
+//             var cTypeID;
+//             if (!!data[i].ContentTypeId) cTypeID = data[i].ContentTypeId.substring(0, 6);
+
+//             if (cTypeID === "0x0101") {
+//                 //declare vars to hold different metadata
+//                 var type,
+//                     fileName,
+//                     documentType,
+//                     fiscalYear,
+//                     recordCode,
+//                     createdBy,
+//                     modifiedBy,
+//                     created,
+//                     modified,
+//                     absURL;
+
+//                 //assign metadata accordingly
+//                 if (!!data[i].File_x0020_Type) type = data[i].File_x0020_Type;
+
+//                 if (!!data[i].FileLeafRef) fileName = $SP().cleanResult(data[i].FileLeafRef);
+
+//                 if (!!data[i][sName]) documentType = data[i][sName];
+
+//                 if (!!data[i].FY) fiscalYear = data[i].FY;
+
+//                 if (!!data[i].TRIM) recordCode = data[i].TRIM;
+
+//                 if (!!data[i].Editor.email) modifiedBy = data[i].Editor.email;
+
+//                 if (!!data[i].Author.email) createdBy = data[i].Author.email;
+
+//                 if (!!data[i].Created_x0020_Date.lookupValue) created = data[i].Created_x0020_Date.lookupValue.substring(0, 10);
+
+//                 if (data[i].Last_x0020_Modified.lookupValue) modified = data[i].Last_x0020_Modified.lookupValue.substring(0, 10);
+
+//                 if (data[i].FileRef.lookupValue) absURL = data[i].FileRef.lookupValue;
+
+//                 //log a preview of the file
+
+//                 // console.log("Type: " + type + "\n" +
+//                 //     "Name: " + fileName + "\n" +
+//                 //     "Document Type: " + documentType + "\n" +
+//                 //     "Fiscal Year: " + fiscalYear + "\n" +
+//                 //     "RSC: " + recordCode + "\n" +
+//                 //     "Modified By: " + modifiedBy + "\n" +
+//                 //     "Created By: " + createdBy + "\n" +
+//                 //     "Created: " + created + "\n" +
+//                 //     "Modified: " + modified + "\n" +
+//                 //     "URL: " + absURL
+//                 // );
+
+//                 //return fields thad only match the "Document" content type, which has id of 0x0101
+//                 if (
+//                     type !== "master" &&
+//                     type !== "aspx" &&
+//                     type !== "png" &&
+//                     type !== "gif" &&
+//                     type !== "xsl" &&
+//                     type !== "xoml" &&
+//                     type !== "jpg" &&
+//                     type !== "xsn" &&
+//                     type !== "css" &&
+//                     type !== "xaml" &&
+//                     type !== "rules"
+//                 ) {
+
+//                     rowNumber += 1;
+//                     rows.push(rowNumber);
+
+//                     switch (documentType) {
+//                         case 'Active Record':
+//                             activeRecs.push('a');
+//                             break;
+//                         case 'Inactive Record':
+//                             inactiveRecs.push('i');
+//                             break;
+//                         case 'Non-Record':
+//                             nonRecs.push('n');
+//                             break;
+//                         case 'Unspecified':
+//                             unspecRecs.push('u');
+//                             break;
+//                         default:
+//                             otherDocs.push('o');
+//                     }
+
+//                     if (type !== undefined) {
+//                         ep.write({
+//                             "cell": "A" + (rows.length + 1),
+//                             "content": type
+//                         });
+//                     }
+
+//                     if (fileName !== undefined) {
+//                         ep.write({
+//                             "cell": "B" + (rows.length + 1),
+//                             "content": fileName
+//                         });
+//                     }
+
+//                     if (documentType !== undefined) {
+//                         ep.write({
+//                             "cell": "C" + (rows.length + 1),
+//                             "content": documentType
+//                         });
+//                     }
+
+//                     if (fiscalYear !== undefined) {
+//                         ep.write({
+//                             "cell": "D" + (rows.length + 1),
+//                             "content": fiscalYear
+//                         });
+//                     }
+
+//                     if (recordCode !== undefined) {
+//                         ep.write({
+//                             "cell": "E" + (rows.length + 1),
+//                             "content": recordCode
+//                         });
+//                     }
+
+//                     if (createdBy !== undefined) {
+//                         ep.write({
+//                             "cell": "F" + (rows.length + 1),
+//                             "content": createdBy + ""
+//                         });
+//                     }
+
+//                     if (modifiedBy !== undefined) {
+//                         ep.write({
+//                             "cell": "G" + (rows.length + 1),
+//                             "content": modifiedBy + ""
+//                         });
+//                     }
+
+//                     if (created !== undefined) {
+//                         ep.write({
+//                             "cell": "H" + (rows.length + 1),
+//                             "content": created + ""
+//                         });
+//                     }
+
+//                     if (modified !== undefined) {
+//                         ep.write({
+//                             "cell": "I" + (rows.length + 1),
+//                             "content": modified + ""
+//                         });
+//                     }
+
+//                     if (absURL !== undefined) {
+//                         ep.write({
+//                             "cell": "J" + (rows.length + 1),
+//                             "content": absURL + ""
+//                         });
+//                     }
+
+//                 }
+//                 //create new list item element
+//                 //var li = "<li class='collection-item avatar'><i class='mdi-file-folder circle blue'></i>" +
+//                 //    "<span class='title'><b>" + fileName + "</b></span>" +
+//                 //    "<p><b>Created by: </b>" + createdBy + "<br>" +
+//                 //    "<b>Modified by: </b>" + modifiedBy + "</p>" +
+//                 //    "<a href='" + absURL + "' class='secondary-content'><i class='mdi-file-file-download'></i></a>" +
+//                 //    "</li>";
+
+//                 //get the ordered list and append the list item, doesn't currently exist in html
+//                 //$("#docs").append(li);
+//             }
+//             //TODO
+//             //push li variable to the jStorage local storage when created
+//             //it will allow the users to browse the files
+//         }
+//         console.log("==============finished for library================");
+//         //procdLists.push('d');
+//         $.p.set(progress(procdLists, listCount));
+//         if (progress(procdLists, listCount) < 100) {
+//             $("#getFilesBtn").attr("class", "btn disabled");
+//             $("#downloadReportBtn").attr("class", "btn-floating btn-large disabled");
+//             $("#downloadReportBtn").unbind("click");
+//             $("#cancelProgress").show();
+//             $("#cancelProgress").click(function(e) {
+//                 e.preventDefault();
+//             });
+//         } else {
+//             executeFileSave();
+//         }
+//     };
+// }
+
 function getDocuments(url, recType, staticName) {
+
+    // var camlViewFields = "<ViewFields>" +
+    //     "<FieldRef Name='File_x0020_Type'/>" +
+    //     "<FieldRef Name='ContentTypeId'/>" +
+    //     "<FieldRef Name='FileExtension'/>" +
+    //     "<FieldRef Name='FileLeafRef'/>" +
+    //     "<FieldRef Name='" + staticName + "'/>" +
+    //     "<FieldRef Name='FY'/>" +
+    //     "<FieldRef Name='TRIM'/>" +
+    //     "<FieldRef Name='Editor'/>" +
+    //     "<FieldRef Name='Author'/>" +
+    //     "<FieldRef Name='Created_x0020_Date'/>" +
+    //     "<FieldRef Name='Last_x0020_Modified'/>" +
+    //     "<FieldRef Name='FileRef'/>" +
+    //     "</ViewFields>";
+
     $SP().lists({
         url: url
     }, function(list) {
+
         for (var i = 0; i < list.length; i++) {
+
+            // $().SPServices({
+            //     webURL: url,
+            //     operation: "GetListItems",
+            //     async: true,
+            //     listName: list[i].Name,
+            //     CAMLViewFields: camlViewFields,
+            //     completefunc: function(xData, Status) {
+            //         var myJson = $(xData.responseXML).SPFilterNode("z:row").SPXmlToJson({
+            //             mapping: {
+            //                 ows_FileLeafRef: {
+            //                     mappedName: "Name",
+            //                     objectType: "Text"
+            //                 },
+            //                 ows_Created_x0020_Date: {
+            //                     mappedName: "Created",
+            //                     objectType: "Text"
+            //                 },
+            //                 ows_Author: {
+            //                     mappedName: "Author",
+            //                     objectType: "User"
+            //                 },
+            //                 ows_ContentTypeId: {
+            //                     mappedName: "ctypeID",
+            //                     objectType: "Text"
+            //                 },
+            //                 ows_File_x0020_Type: {
+            //                     mappedName: "FileExtension",
+            //                     objectType: "Text"
+            //                 },
+            //                 ows_EncodedAbsUrl: {
+            //                     mappedName: "URL",
+            //                     objectType: "Text"
+            //                 }
+            //             }, // name, mappedName, objectType
+            //             includeAllAttrs: false
+            //         });
+            //     console.log(myJson);
+            //     procdLists.push('d');
+            //     }
+            // });
+
+            // var listPromise = $().SPServices.SPGetListItemsJson({
+            //     webURL: url,
+            //     listName: list[i].Name,
+            //     CAMLViewFields: camlViewFields,
+            //     CAMLQueryOptions: "<ExpandUserField>True</ExpandUserField>",
+            //     // mapping: null,
+            //     // mappingOverrides: null,
+            //     debug: true
+            // });
+
+            // $.when(listPromise).then(getDocInfo());
+
             listCount.push(list[i].Name);
             if (recType !== "All Types") {
                 $SP().list(list[i].Name, url).get({
