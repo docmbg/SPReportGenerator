b51c60031e604bb7b07f42ff8aec3d85aad652de
simeonoff <simeonoff@me.com>
2015-04-15 11:55:02 +0300

    removed old report.js file

--- js/reportGenerator.js
+++ js/reportGenerator.js
@@ -29,7 +29,7 @@ var subsites = [],
     unspecRecs = [],
     nonRecs = [],
     otherDocs = [],
-    VERSION = "ver. 0.1b";
+    VERSION = "ver. 0.2b";
 
 function getPercent(num, total) {
     var result = 0;
