880c73c646b8f46f95819feb48bced446e59f342
simeonoff <simeonoff@me.com>
2015-04-16 18:14:23 +0300

    changed absolute links from cdn to rawgit

--- js/saveFileWorker.js
+++ js/saveFileWorker.js
@@ -1,4 +1,4 @@
-﻿importScripts("https://cdn.rawgit.com/docmbg/SPReportGenerator/1.0/libraries/xlsx.core.min.js");
+﻿importScripts("https://rawgit.com/docmbg/SPReportGenerator/1.0/libraries/xlsx.core.min.js");
 
 onmessage = function (epObject) {
 
