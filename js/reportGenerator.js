f2306522eb2c58aaea97475f06c31833c35a177e
simeonoff <simeonoff@me.com>
2015-04-16 18:27:59 +0300

    updated version number

--- js/reportGenerator.js
+++ js/reportGenerator.js
@@ -29,7 +29,7 @@ var subsites = [],
     unspecRecs = [],
     nonRecs = [],
     otherDocs = [],
-    VERSION = "ver. 0.2b";
+    VERSION = "v1.0b";
 
 function getPercent(num, total) {
     var result = 0;
