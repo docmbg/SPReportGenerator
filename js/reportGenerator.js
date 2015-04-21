a5ab6b83b271fa4cddd82b1c4244d0dad5f847e7
simeonoff <simeonoff@me.com>
2015-04-21 18:11:42 +0300

    changed urls from absolute back to relative for testing purposes

--- js/reportGenerator.js
+++ js/reportGenerator.js
@@ -183,8 +183,8 @@ var getFiles = function() {
 $(document).ready(function() {
     $('select').material_select();
     $('.modal-trigger').leanModal();
-    $("#instrContent").load("https://rawgit.com/docmbg/SPReportGenerator/1.0/helpers/instructions.html");
-    $("#changelogContent").load("https://rawgit.com/docmbg/SPReportGenerator/1.0/helpers/changelog.html");
+    $("#instrContent").load("../helpers/instructions.html");
+    $("#changelogContent").load("../helpers/changelog.html");
     $("#version").find(">a").html(VERSION);
     $(".button-collapse").sideNav();
     $("#cancelProgress").hide();
